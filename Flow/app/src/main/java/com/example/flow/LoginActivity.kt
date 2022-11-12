18868da6007ce9b862ffd660ec4a1358e7956752
VIVILL <15170211043@163.com>
2022-11-12 14:05:33 +0800

    使用repeatOnLifecycle替换launchWhenStarted

--- Flow/app/src/main/java/com/example/flow/LoginActivity.kt
+++ Flow/app/src/main/java/com/example/flow/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.example.flow
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.lifecycle.Lifecycle
@@ -30,58 +31,72 @@ class LoginActivity : AppCompatActivity() {
                 binding.etPassword.text.toString())
         }
 
-        lifecycleScope.launchWhenStarted {
-            viewModel.loginUiState.collect {
-                when (it) {
-                    is LoginUiState.Success -> {
-                        Snackbar.make(
-                            binding.root,
-                            "Successfully logged in",
-                            Snackbar.LENGTH_LONG
-                        ).show()
-                        binding.progressBar.isVisible = false
+        lifecycleScope.launch {
+            // https://developer.android.com/kotlin/flow/stateflow-and-sharedflow?hl=zh-cn
+            // 如果需要更新界面，切勿使用 launch 或 launchIn 扩展函数从界面直接收集数据流。
+            // 即使 View 不可见，这些函数也会处理事件。此行为可能会导致应用崩溃。
+            // 为避免这种情况，请使用 repeatOnLifecycle API
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.loginUiState.collect {
+                    when (it) {
+                        is LoginUiState.Success -> {
+                            Snackbar.make(
+                                binding.root,
+                                "Successfully logged in",
+                                Snackbar.LENGTH_LONG
+                            ).show()
+                            binding.progressBar.isVisible = false
+                        }
+                        is LoginUiState.Error -> {
+                            Snackbar.make(
+                                binding.root,
+                                it.message,
+                                Snackbar.LENGTH_LONG
+                            ).show()
+                            binding.progressBar.isVisible = false
+                        }
+                        is LoginUiState.Loading -> {
+                            binding.progressBar.isVisible = true
+                        }
+                        else -> Unit
                     }
-                    is LoginUiState.Error -> {
-                        Snackbar.make(
-                            binding.root,
-                            it.message,
-                            Snackbar.LENGTH_LONG
-                        ).show()
-                        binding.progressBar.isVisible = false
-                    }
-                    is LoginUiState.Loading -> {
-                        binding.progressBar.isVisible = true
-                    }
-                    else -> Unit
                 }
+
             }
         }
 
-        lifecycleScope.launchWhenStarted {
-            viewModel.loginUiAction.collect {
-                when (it) {
-                    is LoginUiAction.Success -> {
-                        Snackbar.make(
-                            binding.root,
-                            "Successfully logged in",
-                            Snackbar.LENGTH_LONG
-                        ).show()
-                        binding.progressBar.isVisible = false
+        // Jetpack MVVM 常见错误二：在 launchWhenX 中启动协程  
+        // https://www.jianshu.com/p/567b19cd5ebf
+        lifecycleScope.launch {
+            // https://developer.android.com/kotlin/flow/stateflow-and-sharedflow?hl=zh-cn
+            // 如果需要更新界面，切勿使用 launch 或 launchIn 扩展函数从界面直接收集数据流。
+            // 即使 View 不可见，这些函数也会处理事件。此行为可能会导致应用崩溃。
+            // 为避免这种情况，请使用 repeatOnLifecycle API
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.loginUiAction.collect {
+                    when (it) {
+                        is LoginUiAction.Success -> {
+                            Snackbar.make(
+                                binding.root,
+                                "Successfully logged in",
+                                Snackbar.LENGTH_LONG
+                            ).show()
+                            binding.progressBar.isVisible = false
+                        }
+                        is LoginUiAction.Error -> {
+                            Snackbar.make(
+                                binding.root,
+                                it.message,
+                                Snackbar.LENGTH_LONG
+                            ).show()
+                            binding.progressBar.isVisible = false
+                        }
+                        is LoginUiAction.Loading -> {
+                            binding.progressBar.isVisible = true
+                        }
+                        else -> Unit
                     }
-                    is LoginUiAction.Error -> {
-                        Snackbar.make(
-                            binding.root,
-                            it.message,
-                            Snackbar.LENGTH_LONG
-                        ).show()
-                        binding.progressBar.isVisible = false
-                    }
-                    is LoginUiAction.Loading -> {
-                        binding.progressBar.isVisible = true
-                    }
-                    else -> Unit
                 }
-
             }
         }
 
