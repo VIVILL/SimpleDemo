3f8193450be5f6effec4ee8c807105b1b2142962
VIVILL <20674096+VIVILL@users.noreply.github.com>
2022-12-05 17:18:46 +0800

    代码优化

--- Banner/app/src/main/java/com/example/banner/adapter/HeaderAdapter.kt
+++ Banner/app/src/main/java/com/example/banner/adapter/HeaderAdapter.kt
@@ -20,37 +20,10 @@ class HeaderAdapter(private val adapter: HeaderItemAdapter) :
     RecyclerView.Adapter<HeaderAdapter.ViewHolder>() {
 
     class ViewHolder(private val binding: ItemParentHeaderBinding) : RecyclerView.ViewHolder(binding.root){
-
-        private lateinit var onViewAttachedListener: () -> Unit
-        private lateinit var onViewDetachedListener: ()  -> Unit
         private lateinit var onTouchEventListener: (ev: MotionEvent)  -> Unit
-
-        fun setOnViewAttachedListener(onViewAttachedListener: () -> Unit) {
-            this.onViewAttachedListener = onViewAttachedListener
-        }
-
-        fun setOnViewDetachedListener(onViewDetachedListener : ()  -> Unit){
-            this.onViewDetachedListener = onViewDetachedListener
-        }
 
         fun setTouchEventListener(onTouchEventListener : (ev: MotionEvent)  -> Unit){
             this.onTouchEventListener = onTouchEventListener
-        }
-
-        init {
-            itemView.addOnAttachStateChangeListener(object : View.OnAttachStateChangeListener {
-                override fun onViewDetachedFromWindow(v: View?) {
-                    //   itemView.removeOnAttachStateChangeListener(this)
-                    Log.d(TAG,"inner onViewAttachedToWindow position = ${binding.viewPager.bannerPosition}")
-                    onViewDetachedListener()
-                }
-
-                override fun onViewAttachedToWindow(v: View?) {
-                    Log.d(TAG,"inner onViewAttachedToWindow position = ${binding.viewPager.bannerPosition}")
-                    binding.viewPager.setPosition(binding.viewPager.bannerPosition)
-                    onViewAttachedListener()
-                }
-            })
         }
 
         fun bind(adapter: HeaderItemAdapter) {
@@ -87,11 +60,21 @@ class HeaderAdapter(private val adapter: HeaderItemAdapter) :
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.setOnViewAttachedListener(onViewAttachedListener)
-        holder.setOnViewDetachedListener(onViewDetachedListener)
         holder.setTouchEventListener(onTouchEventListener)
-
         holder.bind(adapter)
+    }
+
+    override fun onViewAttachedToWindow(holder: ViewHolder) {
+        Log.d(TAG,"inner onViewAttachedToWindow")
+        super.onViewAttachedToWindow(holder)
+        binding.viewPager.setPosition(binding.viewPager.bannerPosition)
+        onViewAttachedListener()
+    }
+
+    override fun onViewDetachedFromWindow(holder: ViewHolder) {
+        Log.d(TAG,"inner onViewDetachedFromWindow")
+        super.onViewDetachedFromWindow(holder)
+        onViewDetachedListener()
     }
 
     override fun getItemCount(): Int = 1
--- Banner/slideconflict/src/main/java/com/example/slideconflict/adapter/HeaderAdapter.kt
+++ Banner/slideconflict/src/main/java/com/example/slideconflict/adapter/HeaderAdapter.kt
@@ -15,37 +15,13 @@ class HeaderAdapter(private val adapter: HeaderItemAdapter) :
 
     class ViewHolder(private val binding: ItemParentHeaderBinding) : RecyclerView.ViewHolder(binding.root){
 
-        private lateinit var onViewAttachedListener: () -> Unit
-        private lateinit var onViewDetachedListener: ()  -> Unit
         private lateinit var onTouchEventListener: (ev: MotionEvent)  -> Unit
 
-        fun setOnViewAttachedListener(onViewAttachedListener: () -> Unit) {
-            this.onViewAttachedListener = onViewAttachedListener
-        }
-
-        fun setOnViewDetachedListener(onViewDetachedListener : ()  -> Unit){
-            this.onViewDetachedListener = onViewDetachedListener
-        }
 
         fun setTouchEventListener(onTouchEventListener : (ev: MotionEvent)  -> Unit){
             this.onTouchEventListener = onTouchEventListener
         }
 
-        init {
-            itemView.addOnAttachStateChangeListener(object : View.OnAttachStateChangeListener {
-                override fun onViewDetachedFromWindow(v: View?) {
-                    //   itemView.removeOnAttachStateChangeListener(this)
-                    Log.d(TAG,"inner onViewAttachedToWindow position = ${binding.viewPager.bannerPosition}")
-                    onViewDetachedListener()
-                }
-
-                override fun onViewAttachedToWindow(v: View?) {
-                    Log.d(TAG,"inner onViewAttachedToWindow position = ${binding.viewPager.bannerPosition}")
-                    binding.viewPager.setPosition(binding.viewPager.bannerPosition)
-                    onViewAttachedListener()
-                }
-            })
-        }
 
         fun bind(adapter: HeaderItemAdapter) {
             Log.d(TAG,"inner bind")
@@ -81,11 +57,21 @@ class HeaderAdapter(private val adapter: HeaderItemAdapter) :
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.setOnViewAttachedListener(onViewAttachedListener)
-        holder.setOnViewDetachedListener(onViewDetachedListener)
         holder.setTouchEventListener(onTouchEventListener)
-
         holder.bind(adapter)
+    }
+
+    override fun onViewAttachedToWindow(holder: ViewHolder) {
+        Log.d(TAG,"inner onViewAttachedToWindow")
+        super.onViewAttachedToWindow(holder)
+        binding.viewPager.setPosition(binding.viewPager.bannerPosition)
+        onViewAttachedListener()
+    }
+
+    override fun onViewDetachedFromWindow(holder: ViewHolder) {
+        Log.d(TAG,"inner onViewDetachedFromWindow")
+        super.onViewDetachedFromWindow(holder)
+        onViewDetachedListener()
     }
 
     override fun getItemCount(): Int = 1
